da08f01a61e1851107db069c3bb160105a271151
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-04-07 09:21:55 +0700

    Add project files.

---
+++ EASD_02/EASD_02.cpp
@@ -0,0 +1,13 @@
+/*1.	Dalam pemrograman komputer, algoritma digunakan untuk memecahkan berbagai masalah, seperti pengurutan data, pencarian data, optimisasi, dan banyak lagi. 
+		Dengan menggunakan algoritma, programer dapat membuat program yang lebih efisien, dapat diandalkan, dan mudah dipelajari dan dimodifikasi.
+		Algoritma itu seperti resep masak, kita butuh resep untuk memasak makanan yang enak dan sama halnya dengan algoritma, kita butuh langkah-langkah yang 
+		terstruktur untuk menyelesaikan masalah dengan benar dan efisien. Tanpa algoritma, seperti memasak tanpa resep, kita mungkin bisa mendapatkan hasil yang 
+		buruk atau tidak konsisten.*/
+/*2.	Static (Array) dan Dynamic (Linked List)*/
+/*3.	Ukuran Input, Kompleksitas Algoritma, Jenis Operasi, Lingkungan Eksekusi, Kualitas Implementasi*/
+/*4.	Jika datanya cukup besar, maka algoritma pengurutan seperti Quick Sort bisa menjadi pilihan yang baik selain Merge Sort. Algoritma Quick Sort memiliki
+		kompleksitas waktu yang sama dengan Merge Sort, yaitu O(n log n), namun terkadang dapat lebih cepat karena menggunakan pendekatan yang berbeda. Merge Sort memiliki
+		kompleksitas waktu yang sama dengan Quick Sort, namun lebih cocok digunakan untuk mengurutkan data dalam jumlah yang sangat besar. */
+/*5.	Algoritma Bubble Sort, Selection Sort, Insertion Sort, dan Shell Sort memiliki kompleksitas waktu O(n^2), yang termasuk dalam kategori algoritma quadratic. 
+		Sedangkan Merge Sort dan Quick Sort memiliki kompleksitas waktu O(n log n), yang termasuk dalam kategori algoritma loglinear.*/
+//Berikut adalah program merge sort
